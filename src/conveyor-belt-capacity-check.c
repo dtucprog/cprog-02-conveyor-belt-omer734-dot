8acfcc5031ddac477c2be4ea725d7c97a757e239
Ömer Ürper <s265440@dtu.dk>
2026-09-13 10:26:20 +0200

    Refactor motor capacity check in conveyor belt program

--- src/conveyor-belt-capacity-check.c
+++ src/conveyor-belt-capacity-check.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 
-int MOTOR_CAPACITY = 12;
+int main(void)
+{
+    const int MOTOR_CAPACITY=12;
+    int motorCount;
+    int totalPackageWeight;
+  // Ask for the number of mototrs
+    printf("How many motors are carrying the packages?\n");
+    scanf("%d", &motorCount);
 
-int main() {
-    int motorCount, totalPackageWeight;
+    // Ask for the number of packages
+    printf("How many kg of packages do we expect?");
+    scanf("%d", &totalPackageWeight);
 
-    /// YOUR CODE HERE
+    // Check whether the boxes have enough capacity
+    if ( totalPackageWeight<= motorCount * MOTOR_CAPACITY)
+    {
+    printf("Yes! The conveyor belt can carry the packages.\n");
+    }
+    else
+    {
+        printf("No. The conveyor belt cannot carry the packages.\n");
+    }
+    return 0;
 }
